e29e40a534da1fa0ee4e2bf5cb2a7440d14abc96
Valeriy Krutoy <valeriy.krutoy@gmail.com>
2018-03-30 18:31:25 +0000

    Java

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const a = parseFloat(prompt('Enter a'));
+const b = parseFloat(prompt('Enter b'));
+const c = parseFloat(prompt('Enter c'));
+
+const result = solveQuard(a, b, c);
+document.write(result);
+
+function solveQuard(a, b, c) {
+    const discr = calcD(a, b, c);
+    if (discr > 0) {
+
+        const x1 = (-b + Math.sqrt(discr)) / (2 * a);
+        const x2 = (-b - Math.sqrt(discr)) / (2 * a);
+        return 'x1 = ' + x1 + ', x2 = ' + x2;
+
+    }
+    if (discr < 0) {
+        return ('no win');
+
+    }
+    if (discr == 0) {
+        const x1 = (-b) / (2 * a);
+    }
+    return ('x = ' + x1);
+
+}
+
+function calcD(a, b, c) {
+    return b ** 2 - 4 * a * c;
+}
